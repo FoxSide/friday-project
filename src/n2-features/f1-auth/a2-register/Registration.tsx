2ad5814e5cacd27e2b7aab70aacf202c995dd526
Rombeso <larionovra@gmail.com>
2022-03-08 16:48:51 +0300

    Added JSX.

--- src/n2-features/f1-auth/a2-register/Registration.tsx
+++ src/n2-features/f1-auth/a2-register/Registration.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import SuperInputText from "../../../n1-main/m1-ui/common/super-input-text/SuperInputText";
+import SuperButton from "../../../n1-main/m1-ui/common/super-button/SuperButton";
 
 const Registration = () => {
   return (
     <div>
-      Registration
-        Committttt
+        <div>
+            <h2>It-incubator</h2>
+            <h3>Sing Up</h3>
+        </div>
+        <div>
+            <p>Email</p>
+            <SuperInputText/>
+            <p>Password</p>
+            <SuperInputText/>
+            <p>Confirm Password</p>
+            <SuperInputText/>
+        </div>
+        <div>
+            <SuperButton>Cancel</SuperButton>
+            <SuperButton>Register</SuperButton>
+        </div>
+
     </div>
   );
 };
