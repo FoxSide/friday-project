4027b1cb0215fa17e7dec303f463342f48f0fb82
FoxSide <foxside8452@gmail.com>
2022-03-17 18:48:00 +0200

    delete test

--- src/n1-main/m1-ui/routes/Routes.tsx
+++ src/n1-main/m1-ui/routes/Routes.tsx
@@ -5,7 +5,6 @@ import Registration from "../../../n2-features/f1-auth/a2-register/Registration"
 import Profile from "../../../n2-features/f2-profile/Profile";
 import ErrorPage from "../../../n2-features/f3-error-page/ErrorPage";
 import PasswordRecovery from "../../../n2-features/f1-auth/a5-password-recovery/passwordRecowery/PasswordRecovery";
-import Test from "../../../n2-features/f0-test/Test";
 import ProfileEdit from '../../../n2-features/f2-profile/ProfileEdit';
 import s from './routes.module.css'
 import {useDispatch, useSelector} from "react-redux";
@@ -24,7 +23,8 @@ export const path = {
   passwordRecovery: '/password-recovery',
   checkEmail: '/check-email',
   newPassword: '/new-password/:token',
-  test: '/test'
+  test: '/test',
+  cards: '/cards'
 }
 
 
@@ -43,12 +43,12 @@ const RoutesComponent = () => {
         <Route path={path.registration} element={<Registration/>}/>
         <Route path={path.profile} element={<Profile/>}/>
         {/*<Route path={path.profile} element={<PackContainer/>}/>*/}
+        <Route path={path.cards} element={<PackContainer/>}/>
         <Route path={path.profileEdit} element={<ProfileEdit/>}/>
         <Route path={path.errorPage} element={<ErrorPage/>}/>
         <Route path={path.passwordRecovery} element={<PasswordRecovery/>}/>
         <Route path={path.checkEmail} element={<CheckEmail/>}/>
         <Route path={path.newPassword} element={<CreateNewPassword/>}/>
-        <Route path={path.test} element={<Test/>}/>
       </Routes>
     </div>
   );
